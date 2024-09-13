cdd2f47f0ce68104a9598cd83b84367ae38d2da0
takumi <ganndamu0706@gmail.com>
2024-09-14 02:00:52 +0900

    feat:Add Middleware.ts creation for Edge, RegisterForm creation, DB connection by Prisma, PostgratSQL creation by Neon, Salt+Hash for password rainbow table attack protection, Table schema creation, Now implementing auth function.

--- demo-next-js/actions/login.ts
+++ demo-next-js/actions/login.ts
@@ -2,7 +2,6 @@
 import * as z from "zod";
 
 import {LoginSchema} from "@/schemas";
-import {RegisterSchema} from "@/schemas";
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values);
 
