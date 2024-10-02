faadd77d7caf19c212372ef10db9eb025557fa7b
takumi <ganndamu0706@gmail.com>
2024-10-02 20:33:17 +0900

    ✨ feat: ダイアログコンポーネントを追加し、設定ページのフォームを改良 🔧 fix: メール送信機能を更新し、2要素認証の設定を追加 📦 chore: Shadcn UIの新しいパッケージを追加し、依存関係を更新 📝 docs: 設定スキーマを修正し、新しいパラメータを追加

--- nextjs/actions/settings.ts
+++ nextjs/actions/settings.ts
@@ -4,11 +4,14 @@ import * as z from "zod";
 
 import {db} from "@/lib/db";
 import {SettingsSchema} from "@/schemas";
-import {getUserById} from "@/data/user";
+import {getUserByEmail, getUserById} from "@/data/user";
 import {currentUser} from "@/lib/auth";
+import {generateVerificationToken} from "@/lib/tokens";
+import {sendVerificationEmail} from "@/lib/mail";
+import bcryptjs from "bcryptjs";
 
 export const settings = async (
-    value: z.infer<typeof SettingsSchema>
+    values: z.infer<typeof SettingsSchema>
 ) => {
     const user = await currentUser();
 
@@ -22,10 +25,54 @@ export const settings = async (
         return { error: "Unauthorized" };
     }
 
+    if(user.isOAuth){
+        values.email = undefined;
+        values.password = undefined;
+        values.newPassword = undefined;
+        values.isTwoFactorEnabled = undefined;
+    }
+
+    if (values.email && values.email !== user.email) {
+        const existingUser = await getUserByEmail(values.email);
+        if (existingUser && existingUser.id !== user.id) {
+            return { error: "Email already in use!" };
+        }
+
+        const verificationToken = await generateVerificationToken(
+            values.email
+        );
+        await sendVerificationEmail(
+            verificationToken.email,
+            verificationToken.token
+        );
+
+        return { success: "Verification email sent!" };
+    }
+
+    if (values.password && values.newPassword && dbUser.password) {
+        const passwordMatch = await bcryptjs.compare(
+            values.password,
+            dbUser.password
+        );
+
+        if (!passwordMatch) {
+            return { error: "Incorrect password!" };
+        }
+
+        const saltRounds = 10;
+        const salt = await bcryptjs. genSalt(saltRounds);
+        const hashedPassword = await bcryptjs.hash(
+            values.newPassword,
+            salt
+        );
+        values.password = hashedPassword;
+        values.newPassword = undefined;
+    }
+
     await db.user.update({
         where: { id: user.id },
         data: {
-            ...value,
+            ...values,
         },
     });
 
--- nextjs/schemas/index.ts
+++ nextjs/schemas/index.ts
@@ -1,8 +1,34 @@
 import * as z from 'zod';
+import { UserRole } from '@prisma/client';
 
 export const SettingsSchema = z.object({
     name: z.optional(z.string()),
-});
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.USER, UserRole.ADMIN]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(8)),
+    newPassword: z.optional(z.string().min(8)),
+})
+    .refine((data) => {
+        if (data.password && !data.newPassword) {
+            return false;
+        }
+
+        return true;
+    },{
+        message: "New password is required!",
+        path: ["newPassword"]
+    })
+    .refine((data) => {
+        if (data.newPassword && !data.password) {
+            return false;
+        }
+
+        return true;
+    },{
+        message: "Password is required!",
+        path: ["Password"]
+    })
 
 export const NewPasswordSchema = z.object({
     password: z.string().min(8, {
